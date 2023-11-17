f092ba68e58fb877b6746b36aee3fd307cc08d9f
Brittany Brimley <brittanysapp@gmail.com>
2023-11-16 17:51:31 -0700

    added me, createUser, loginUser, saveBook, and removeBook to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,53 @@
 const { User, Book } = require('../models');
-const { AuthenticationError } = require('apollo-server');
-const { signToken } = require('../utils/auth');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 // Matches the typeDefs entry point and informs the request of the relevant data
 const resolvers = {
     Query: {
-        user: async () => {
-            return User.find({});
-        },
-        books: async (parent, { _id }) => {
-            const params = _id ? { _id } : {};
-            return Book.find(params);
+        me: async (parent, { username }) => {
+            return User.findOne({ username });
         },
     },
     Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        loginUser: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { bookData, bookId }) => {
+            const book = await Book.create({ bookData, bookId });
+
+            await User.findOneAndUpdate(
+                { username: bookData },
+                { $addToSet: { books: book._id } }
+            );
+
+            return book;
+        },
+        removeBook: async (parent, { bookId, bookData }) => {
+            return Book.findOneAndUpdate(
+                { _id: bookId },
+                { $pull: { books: { _id: bookId } } },
+                { new: true }
+            );
+        },
     },
 };
 
